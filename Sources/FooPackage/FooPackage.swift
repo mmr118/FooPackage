f6c1bc83a0cac370ac1ac4950115f087bdd84624
Monica Rondón <monirondon15@gmail.com>
2022-09-28 18:09:01 +0200

    [master]🏡  Fixes wranings/errors from dated tutorial(?)

--- Sources/FooPackage/FooPackage.swift
+++ Sources/FooPackage/FooPackage.swift
@@ -3,14 +3,31 @@ import Appwrite
 
 public struct FooPackage {
     
-    static let client = Client()
-    static let account = Account(client)
+    static var client = Client()
+    static var account = Account(client)
     
     public static func login(endpoint: String, projId: String, email: String, pw: String, completion: @escaping (Result<Session, AppwriteError>) -> Void) {
-     
-        client.setEndpoint(endpoint).setProject(projId)
+        client.updateEndpoint(endpoint).updateProject(projId)
         
-//        account.createSession(email: email, password: pw, completion: completion)
-        
+        // Account does not seem to have the function createSession(email:password:completion:)
+        // account.createSession(email: email, password: pw, completion: completion)
     }
+}
+
+
+
+public extension Appwrite.Client {
+    
+    @discardableResult
+    func updateEndpoint(_ newEndpoint: String) -> Client {
+        let _ = self.setEndpoint(newEndpoint)
+        return self
+    }
+    
+    @discardableResult
+    func updateProject(_ newProject: String) -> Client {
+        let _ = self.setProject(newProject)
+        return self
+    }
+
 }
