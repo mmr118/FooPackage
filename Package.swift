0be4ae2422b355df9bae75b4f341555ac4a2732a
Monica Rondón <monirondon15@gmail.com>
2022-09-28 12:38:42 +0200

    [master]🏡  Adds dependency {Appwrite} and resolves package

--- Package.swift
+++ Package.swift
@@ -14,13 +14,19 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+
+        // warning: 'package(name:url:from:)' is deprecated: use package(url:from:) instead
+        // .package(name: "Appwrite", url: "https://github.com/appwrite/sdk-for-swift", from: "0.1.0")
+        .package(url: "https://github.com/appwrite/sdk-for-swift", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FooPackage",
-            dependencies: []),
+            dependencies: [
+                "Appwrite"
+            ]),
         .testTarget(
             name: "FooPackageTests",
             dependencies: ["FooPackage"]),
